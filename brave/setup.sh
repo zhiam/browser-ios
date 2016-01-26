09b663289b6747d34a3868aaa1429d7ec946193e
Marshall Rose <mrose17@gmail.com>
2016-01-25 17:30:44 -0800

    move the bundle prefix pro-tip to a better spot

--- brave/setup.sh
+++ brave/setup.sh
@@ -1,10 +1,10 @@
 [[ -e setup.sh  ]] || { echo 'setup.sh must be run from brave directory'; exit 1; }
 
-echo "Tip: add your app id as an arg, like ./setup.sh org.foo.myapp"
-
 app_id=${1:-com.brave.ios.browser}
 echo CUSTOM_BUNDLE_ID=$app_id > xcconfig/.bundle-id.xcconfig
 if [  -z $1 ] ; then
+  echo "Tip: add your app id as an arg, like ./setup.sh org.foo.myapp"
+
   echo DevelopmentTeam = KL8N8XSYF4 >> xcconfig/.bundle-id.xcconfig
 fi
 
