c8b11bb64c10ae0de61968364c95ad98415eb143
Garvan Keeley <garvankeeley@gmail.com>
2016-01-25 20:01:06 -0500

    Disable SpotlightHelper, seems to crash on startup, TODO: investigate if our UIWebView implementation can use this

--- Client/Helpers/SpotlightHelper.swift
+++ Client/Helpers/SpotlightHelper.swift
@@ -47,6 +47,10 @@ class SpotlightHelper: NSObject {
     }
 
     func update(pageContent: [String: String], forURL url: NSURL) {
+        #if BRAVE
+            return
+        #endif
+
         var activity: NSUserActivity
         if let currentActivity = self.activity where currentActivity.webpageURL == url {
             activity = currentActivity
