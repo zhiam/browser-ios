b4aecb142915bed4904f65828abffaf773b4b068
Garvan Keeley <garvankeeley@gmail.com>
2016-01-28 22:54:52 -0500

    HttpsEverywhere rudimentary test case

---
+++ brave/src/test/HttpsEverywhereTest.swift
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+import XCTest
+@testable import Client
+import Shared
+
+class WebViewLoadTest: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func test() {
+        expectationForNotification(HttpsEverywhere.kNotificationDataLoaded, object: nil, handler:nil)
+        HttpsEverywhere.singleton.loadData()
+        var isOk = true
+        waitForExpectationsWithTimeout(5) { (error:NSError?) -> Void in
+            if let _ = error {
+                isOk = false
+                XCTAssert(false, "load data failed")
+            }
+        }
+
+        if !isOk {
+            return
+        }
+
+        let urls = ["thestar.com", "www.thestar.com", "apple.com", "xkcd.com"]
+
+        for url in urls {
+            let redirected = HttpsEverywhere.singleton.tryRedirectingUrl(NSURL(string: "http://" + url)!)
+            XCTAssert(redirected != nil && redirected!.scheme.startsWith("https"), "failed:" + url)
+        }
+    }
+}
