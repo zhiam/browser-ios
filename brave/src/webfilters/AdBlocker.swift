2c281b3e089074869143771362866809155e7005
Garvan Keeley <garvankeeley@gmail.com>
2016-02-25 12:36:49 -0500

    Forbes hack: even with the cookie set, I can't get past the welcome page, hack this by doing a page forward internally

--- brave/src/webfilters/AdBlocker.swift
+++ brave/src/webfilters/AdBlocker.swift
@@ -130,7 +130,14 @@ class AdBlocker {
 
         if url.host?.contains("forbes.com") ?? false {
             setForbesCookie()
+            if url.absoluteString.contains("/forbes/welcome") {
+                delay(0.5) {
+                    /* For some reason, even with the cookie set, I can't get past the welcome page, until I manually load a page on forbes. So if a do a google search for a subpage on forbes, I can click on that and get to forbes, and from that point on, I no longer see the welcome page. This hack seems to work perfectly for duplicating that behaviour. */
+                    BraveApp.getCurrentWebView()?.loadRequest(NSURLRequest(URL: NSURL(string: "http://www.forbes.com")!))
+                }
+            }
         }
+
 
         if request.mainDocumentURL?.absoluteString.startsWith(WebServer.sharedInstance.base) ?? false {
             return false
