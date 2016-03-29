7b86bfdcd1decc403a579a9ea143ad1600f3c4bc
Garvan Keeley <garvankeeley@gmail.com>
2016-03-29 10:33:47 -0400

    Remove call to sharedApplication(), not supposed to use that in extensions. Can search the responder chain for the app instead.

--- BraveShareTo/ShareToBraveViewController.swift
+++ BraveShareTo/ShareToBraveViewController.swift
@@ -3,6 +3,16 @@ import UIKit
 import Social
 import MobileCoreServices
 
+extension NSObject {
+    func callSelector(selector: Selector, object: AnyObject?, delay: NSTimeInterval) {
+        let delay = delay * Double(NSEC_PER_SEC)
+        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+        dispatch_after(time, dispatch_get_main_queue(), {
+            NSThread.detachNewThreadSelector(selector, toTarget:self, withObject: object)
+        })
+    }
+}
+
 class ShareToBraveViewController: SLComposeServiceViewController {
 
     override func isContentValid() -> Bool {
@@ -16,26 +26,34 @@ class ShareToBraveViewController: SLComposeServiceViewController {
     override func configurationItems() -> [AnyObject]! {
         let item: NSExtensionItem = extensionContext!.inputItems[0] as! NSExtensionItem
         let itemProvider: NSItemProvider = item.attachments![0] as! NSItemProvider
-        var url: NSString = ""
         let type = kUTTypeURL as String
+
         if itemProvider.hasItemConformingToTypeIdentifier(type) {
             itemProvider.loadItemForTypeIdentifier(type, options: nil, completionHandler: {
                 (urlItem, error) in
-                url = (urlItem as! NSURL).absoluteString.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.alphanumericCharacterSet())!
-                UIApplication.sharedApplication().openURL(NSURL(string: "brave://open-url?url=\(url)")!)
+                guard let url = (urlItem as! NSURL).absoluteString.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.alphanumericCharacterSet()),
+                 let braveUrl = NSURL(string: "brave://open-url?url=\(url)") else { return }
+
+                // From http://stackoverflow.com/questions/24297273/openurl-not-work-in-action-extension
+                var responder = self as UIResponder?
+                while (responder != nil) {
+                    if responder!.respondsToSelector(Selector("openURL:")) {
+                        responder!.callSelector(Selector("openURL:"), object: braveUrl, delay: 0)
+                    }
+                    responder = responder!.nextResponder()
+                }
+
                 dispatch_after(
                     dispatch_time(
                         DISPATCH_TIME_NOW,
                         Int64(0.1 * Double(NSEC_PER_SEC))
                     ),
                     dispatch_get_main_queue(), { self.cancel() })
-
             })
         }
 
         return []
     }
-
 
     override func viewDidAppear(animated: Bool) {
         // Stop keyboard from showing
