654dc636126a87de8a196b244ac782380d626309
Garvan Keeley <garvankeeley@gmail.com>
2016-04-21 17:11:05 -0400

    Using ruby xcodeproj now (+2 squashed commits) Squashed commits: [c622c04] This is now running, but xcode project needs to be re-written due to bug in project generator [520ef9a] New project generation using lists of files to add to build targets. TODO: remove fx entitlement files

--- brave/src/test/WebViewLoadTest.swift
+++ brave/src/test/WebViewLoadTest.swift
@@ -1,5 +1,5 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
+#if TEST
 import Foundation
 import XCTest
 @testable import Client
@@ -177,3 +177,4 @@ class WebViewLoadTest: XCTestCase {
     XCTAssert(ok, "open url failed for site: \(site)")
   }
 }
+#endif
