ab5228cd87ebb63a311a39b1f7896634ef9866b6
Garvan Keeley <garvankeeley@gmail.com>
2016-09-06 18:40:40 -0400

    Build system: remove BraveInfo.plist, go back to generating from template

--- brave/setup.sh
+++ brave/setup.sh
@@ -6,12 +6,14 @@ app_id=${1:-com.brave.ios.browser}
 echo CUSTOM_BUNDLE_ID=$app_id > xcconfig/.bundle-id.xcconfig
 # Custom IDs get the BETA property set automatically
 [[  -z $1 ]] || echo BETA=Beta >> xcconfig/.bundle-id.xcconfig
-
+echo $app_id
 # if a brave build, setup fabric
-if [ $app_id = com.brave.ios* ]; then
+if [ $app_id = com.brave.ios.browser ]; then
   echo "./Fabric.framework/run $(head -1 ~/.brave-fabric-keys) $(tail -1 ~/.brave-fabric-keys)" > build-system/.fabric-key-setup.sh
+  sed -e s/FABRIC_KEY_REMOVED/$(head -1 ~/.brave-fabric-keys)/ BraveInfo.plist.template > BraveInfo.plist
 else
    >build-system/.fabric-key-setup.sh
+   cat BraveInfo.plist.template > BraveInfo.plist
 fi
 
 sed -e "s/BUNDLE_ID_PLACEHOLDER/$app_id/" Brave.entitlements.template > Brave.entitlements
@@ -19,5 +21,3 @@ sed -e "s/BUNDLE_ID_PLACEHOLDER/$app_id/" Brave.entitlements.template > Brave.en
 npm update
 
 echo GENERATED_BUILD_ID=`date +"%y.%m.%d.%H"`  > xcconfig/build-id.xcconfig
-
-printf 'Please use our commit template, run this to install it\n\n git config commit.template brave/COMMIT_TEMPLATE\n\n'
