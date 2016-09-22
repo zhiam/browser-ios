zhiam/browser-ios

commit 2ad9cb02487f613e92ec6ad1af0d45d659c9781d

Tabs count reported wrong. The tab count widget has tricky async animation code using a cloned widget, and widget isn't guaranteeing to use the current tab count on completion. Changed the code to use the current TabManager tab count when animation completed. I was only able to repro this once, doing rapid tab creation of 10 tabs, then rapid deletion to 2 tabs, and 3 was reported. @diegodoval @alexwykoff

Client/Frontend/Browser/URLBarView.swift
@@ -298,31 +298,22 @@ class URLBarView: UIView {
     }
 
     class func updateTabCount(tabsButton: TabsButton, inout clonedTabsButton: TabsButton?, count: Int, animated: Bool = true) {
-        let currentCount = tabsButton.titleLabel.text
+
+        struct LastCount {
+            static var value = -1
+        }
         // only animate a tab count change if the tab count has actually changed
-        if currentCount == count.description {
+        if LastCount.value > -1 && LastCount.value == count {
             return
         }
-//
-//            // make a 'clone' of the tabs button
-//            let newTabsButton = self.tabsButton.clone() as! TabsButton
-//            self.clonedTabsButton = newTabsButton
-//            newTabsButton.addTarget(self, action: #selector(URLBarView.SELdidClickAddTab), forControlEvents: UIControlEvents.TouchUpInside)
-//            newTabsButton.titleLabel.text = count.description
-//            newTabsButton.accessibilityValue = count.description
-//            addSubview(newTabsButton)
-//            newTabsButton.snp_makeConstraints { make in
-//                make.centerY.equalTo(self.locationContainer)
-//                make.trailing.equalTo(self)
-//                make.size.equalTo(UIConstants.ToolbarHeight)
-//            }
+        LastCount.value = count
 
         // make a 'clone' of the tabs button
         let newTabsButton = tabsButton.clone() as! TabsButton
         clonedTabsButton = newTabsButton
         // BRAVE: see clone(), do not to this here: newTabsButton.addTarget(parent, action: "SELdidClickAddTab", forControlEvents: UIControlEvents.TouchUpInside)
-        newTabsButton.titleLabel.text = count.description
-        newTabsButton.accessibilityValue = count.description
+        newTabsButton.titleLabel.text = "\(count)"
+        newTabsButton.accessibilityValue = "\(count)"
 
         // BRAVE added
         guard let parentView = tabsButton.superview else { return }
@@ -365,10 +356,11 @@ class URLBarView: UIView {
             tabsButton.insideButton.layer.transform = CATransform3DIdentity
             tabsButton.accessibilityLabel = NSLocalizedString("Show Tabs", comment: "Accessibility label for the tabs button in the (top) browser toolbar")
 
-            if finished {
-                tabsButton.titleLabel.text = count.description
-                tabsButton.accessibilityValue = count.description
-            }
+            // By this time, the 'count' func argument may be out of date, use the correct current count
+            let currentCount = getApp().tabManager.tabs.displayedTabsForCurrentPrivateMode.count
+            LastCount.value = currentCount
+            tabsButton.titleLabel.text = "\(currentCount)"
+            tabsButton.accessibilityLabel = "\(currentCount)"
         }
 
         if animated {
@@ -376,7 +368,6 @@ class URLBarView: UIView {
         } else {
             completion(true)
         }
-
     }
 
     func updateProgressBar(progress: Float, dueToTabChange: Bool = false) {

commit e6dedd8b61f27e7ec4fc976b828ae146a4953b91

Tab count portrait iphone is wrong. Earlier today's commit 2ad9cb024 broke this. Fixed, again. @alexwykoff

Client/Frontend/Browser/URLBarView.swift
@@ -298,15 +298,11 @@ class URLBarView: UIView {
     }
 
     class func updateTabCount(tabsButton: TabsButton, inout clonedTabsButton: TabsButton?, count: Int, animated: Bool = true) {
-
-        struct LastCount {
-            static var value = -1
-        }
+        let currentCount = tabsButton.titleLabel.text
         // only animate a tab count change if the tab count has actually changed
-        if LastCount.value > -1 && LastCount.value == count {
+        if currentCount == "\(count)" {
             return
         }
-        LastCount.value = count
 
         // make a 'clone' of the tabs button
         let newTabsButton = tabsButton.clone() as! TabsButton
@@ -358,7 +354,6 @@ class URLBarView: UIView {
 
             // By this time, the 'count' func argument may be out of date, use the correct current count
             let currentCount = getApp().tabManager.tabs.displayedTabsForCurrentPrivateMode.count
-            LastCount.value = currentCount
             tabsButton.titleLabel.text = "\(currentCount)"
             tabsButton.accessibilityLabel = "\(currentCount)"
         }
@@ -489,8 +484,8 @@ class URLBarView: UIView {
             self.transitionToOverlay(cancel)
             self.setNeedsUpdateConstraints()
             self.layoutIfNeeded()
-        }, completion: { _ in
-            self.updateViewsForOverlayModeAndToolbarChanges()
+            }, completion: { _ in
+                self.updateViewsForOverlayModeAndToolbarChanges()
         })
     }
 
@@ -575,7 +570,7 @@ extension URLBarView: BrowserLocationViewDelegate {
     func browserLocationViewDidTapReload(browserLocationView: BrowserLocationView) {
         delegate?.urlBarDidPressReload(self)
     }
-    
+
     func browserLocationViewDidTapStop(browserLocationView: BrowserLocationView) {
         delegate?.urlBarDidPressStop(self)
     }
@@ -629,7 +624,7 @@ extension URLBarView {
 }
 
 extension URLBarView: Themeable {
-    
+
     func applyTheme(themeName: String) {
         locationView.applyTheme(themeName)
         locationTextField?.applyTheme(themeName)
@@ -742,7 +737,7 @@ extension ToolbarTextField: Themeable {
             log.error("Unable to apply unknown theme \(themeName)")
             return
         }
-
+        
         backgroundColor = theme.backgroundColor
         textColor = theme.textColor
         clearButtonTintColor = theme.buttonTintColor