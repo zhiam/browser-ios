a91c7c1b36c39d5580cbb7fe6497d23a0167da03
Garvan Keeley <garvankeeley@gmail.com>
2016-10-12 10:31:48 -0400

    debug-only message to warn about db ops on main thread was inverted

--- Storage/ThirdParty/SwiftData.swift
+++ Storage/ThirdParty/SwiftData.swift
@@ -137,7 +137,7 @@ public class SwiftData {
 
         if synchronous {
             var error: NSError? = nil
-            if !NSThread.isMainThread() {
+            if NSThread.isMainThread() {
                 print("SwiftData synchronous call detected on main thread. Try move these off-main.")
             }
             dispatch_sync(connection.queue) {
