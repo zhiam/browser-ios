23dedd079757f675de8e3b9a87fc4dc3034c2471
Garvan Keeley <garvankeeley@gmail.com>
2016-10-12 17:20:34 -0400

    update build script for Xcode8 automatic provisioning

--- brave/build-system/build-archive.sh
+++ brave/build-system/build-archive.sh
@@ -1,7 +1,6 @@
 [[ $1 == 'release' ]] && bundle='com.brave.ios.browser'
 [[ $1 == 'beta' ]] && bundle='com.brave.ios.browser.dev'
 (cd .. && ./setup.sh $bundle)
-(cd profiles && sh install-profiles-from-portal.sh $1) || exit 1 
 sh strip-arch.sh
 (cd -- "$(dirname -- "$0")" && cd ../.. && \
- xcodebuild archive -scheme Brave CODE_SIGN_IDENTITY="iPhone Distribution: Brave Software, Inc. (KL8N8XSYF4)")
+ xcodebuild archive -scheme Brave )
