e1d5eb361e0b972e2537a47c10e611b3ef8c1722
Garvan Keeley <garvankeeley@gmail.com>
2016-10-12 15:30:06 -0400

    Test target needs to link against Deferred framework

--- Utils/TimeConstants.swift
+++ Utils/TimeConstants.swift
@@ -3,7 +3,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import Foundation
-import Shared
 
 public typealias Timestamp = UInt64
 public typealias MicrosecondTimestamp = UInt64
