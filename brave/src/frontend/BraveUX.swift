16a64ac46da7bcc9d8c8765530a57bd133ef8f07
Garvan Keeley <garvankeeley@gmail.com>
2016-11-09 12:18:53 -0500

    Reader mode is beta-only for now

--- brave/src/frontend/BraveUX.swift
+++ brave/src/frontend/BraveUX.swift
@@ -1,6 +1,8 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 struct BraveUX {
+    static let IsRelease = NSBundle.mainBundle().bundleIdentifier == "com.brave.ios.browser"
+
     static let BravePrivacyURL = NSURL(string: "https://www.brave.com/privacy_ios")!
     static let PrefKeyOptInDialogWasSeen = "OptInDialogWasSeen"
     static let PrefKeyUserAllowsTelemetry = "userallowstelemetry"
